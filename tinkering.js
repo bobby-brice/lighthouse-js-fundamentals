2aa19327259b54caafa62f726baf84e9a60d87db
bobby-brice <bobby.brice@gmail.com>
2020-02-18 00:49:50 +0000

    added ice-cream and tinkering updates

--- tinkering.js
+++ tinkering.js
@@ -51,3 +51,58 @@ var laugh = function (num) {
 };
 
 console.log(laugh(10));
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return name + " " + "is " + age + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+function howManyHundreds(num) {
+  let divide = num / 100;
+  return Math.floor(divide);
+};
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
+
+function calculateRectangleArea(length, width) {
+  if(length < 0 || width < 0) {
+    return undefined;
+  } else {
+    return length * width;
+  } 
+}
+
+function calculateTriangleArea(base, height) {
+  if(base < 0 || height < 0) {
+    return undefined;
+  } else {
+    return base * (height/2);
+  } 
+}
+
+function calculateCircleArea(radius) {
+  if (radius < 0) {
+    return undefined;
+  } else {
+    return Math.PI * Math.pow(radius, 2);
+  }  
+}
+
+console.log(calculateRectangleArea(10, 5)); // should print 50
+console.log(calculateRectangleArea(1.5, 2.5)); // should print 3.75
+console.log(calculateRectangleArea(10, -5)); // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
